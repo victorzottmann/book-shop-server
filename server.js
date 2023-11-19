64d32985a055b206213c69d50b858d6910471a24
Victor Zottmann <zottmann.victor@gmail.com>
2023-11-20 00:11:58 +1100

    add example routes

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http"
 
 const PORT = 3000
 
+const routes = {
+  "/": "Book shop server",
+  "/books": "In the books route",
+  "/authors": "In the authors route"
+}
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Book shop server") // shown at localhost:PORT
+  res.end(routes[req.url]) // req.url returns whichever route is accessed at localhost:PORT
 })
 
 server.listen(PORT, () => {

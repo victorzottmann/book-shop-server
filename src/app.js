c29d29d3339024475f9a365f180392cf1b34c138
Victor Zottmann <zottmann.victor@gmail.com>
2023-11-20 16:25:02 +1100

    add post request to add new book

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from "express"
-
 import books from "./data.js"
 
 const app = express()
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res.status(200).send("Book shop server")
@@ -10,6 +10,11 @@ app.get("/", (req, res) => {
 
 app.get("/books", (req, res) => {
   res.status(200).json(books)
+})
+
+app.post("/books", (req, res) => {
+  books.push(req.body)
+  res.status(201).send("Book added successfully")
 })
 
 export default app
